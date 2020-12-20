b0e7223adfd96cface809fd7a942bd69898a9213
Breanna <breanna@breannawebster.com>
2020-12-20 17:42:45 -0500

    commit tree 904d055ecfd9b45863f7aec2cb062f89e3fdc488

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+    "My name is Bre"
+end
+    get '/hometown' do
+    "My hometown is Nashville"
+end
+
+   get '/favorite-song'do
+   "My favorite song is can we talk"
+end
 end
